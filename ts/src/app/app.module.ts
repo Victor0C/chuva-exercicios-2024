7198617230b1afdd19b3b8167d0a732568049234
Victor Hugo <hugovco511@gmail.com>
2024-04-17 18:52:16 -0300

    feat: header

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { MyHeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideBarComponent,
+    MyHeaderComponent,
   ],
   imports: [
     BrowserModule
