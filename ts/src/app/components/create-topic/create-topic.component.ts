6e3e428c31d52da339df4f72082bb62fa5b0a4a5
Victor Hugo <hugovco511@gmail.com>
2024-04-20 13:14:55 -0300

    feat: componente create-topic

---
+++ ts/src/app/components/create-topic/create-topic.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-topic',
+  templateUrl: './create-topic.component.html',
+  styleUrls: ['./create-topic.component.scss']
+})
+export class CreateTopicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  newTopic(): void {
+    const divCreateTopic = document.getElementById('create-topic') as HTMLElement;
+    const divFillTopic = document.getElementById('fill-topic') as HTMLElement;
+    const divCreatedTopic = document.getElementById('topic-created') as HTMLElement;
+
+    divCreateTopic.style.display = 'none';
+    divFillTopic.style.display = 'flex';
+ 
+  if (divCreatedTopic.style.display === 'flex') {
+    divCreatedTopic.style.display = 'none';
+    }
+  }
+
+}
