8860d3014a9c58c96d7c6116272642ed2a76f6a7
Victor Hugo <hugovco511@gmail.com>
2024-04-21 19:57:16 -0300

    refactor: função de ver mais e ver menos

--- ts/src/app/components/section-summary/section-summary.component.ts
+++ ts/src/app/components/section-summary/section-summary.component.ts
@@ -6,32 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-summary.component.scss']
 })
 export class SectionSummaryComponent implements OnInit {
+  renderMore: boolean = false
 
   constructor() { }
 
   public ngOnInit(): void { }
 
   public viewMore(): void {
-    const ellipsis = document.getElementById('ellipsis') as HTMLElement;
-    const viewmore = document.getElementById('see-more') as HTMLElement;
-    const btnShowMore = document.getElementById('btn-show-more') as HTMLElement;
-    const btnShowLess = document.getElementById('btn-show-less') as HTMLElement;
+    this.renderMore = !this.renderMore
 
-    ellipsis.style.display = 'none';
-    viewmore.style.display = 'flex';
-    btnShowMore.style.display = 'none';
-    btnShowLess.style.display = 'inline';
+    // const ellipsis = document.getElementById('ellipsis') as HTMLElement;
+    // const viewmore = document.getElementById('see-more') as HTMLElement;
+    // const btnShowMore = document.getElementById('btn-show-more') as HTMLElement;
+    // const btnShowLess = document.getElementById('btn-show-less') as HTMLElement;
+
+    // ellipsis.style.display = 'none';
+    // viewmore.style.display = 'flex';
+    // btnShowMore.style.display = 'none';
+    // btnShowLess.style.display = 'inline';
   }
 
   public viewLess(): void {
-    const ellipsis = document.getElementById('ellipsis') as HTMLElement;
-    const viewmore = document.getElementById('see-more') as HTMLElement;
-    const btnShowMore = document.getElementById('btn-show-more') as HTMLElement;
-    const btnShowLess = document.getElementById('btn-show-less') as HTMLElement;
+    // const ellipsis = document.getElementById('ellipsis') as HTMLElement;
+    // const viewmore = document.getElementById('see-more') as HTMLElement;
+    // const btnShowMore = document.getElementById('btn-show-more') as HTMLElement;
+    // const btnShowLess = document.getElementById('btn-show-less') as HTMLElement;
 
-    ellipsis.style.display = 'inline';
-    viewmore.style.display = 'none';
-    btnShowMore.style.display = 'inline';
-    btnShowLess.style.display = 'none';
+    // ellipsis.style.display = 'inline';
+    // viewmore.style.display = 'none';
+    // btnShowMore.style.display = 'inline';
+    // btnShowLess.style.display = 'none';
   }
 }
